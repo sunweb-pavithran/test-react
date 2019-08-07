d02e11c8c3ff9033b74c4371f1a0eef5a5f84ee8
sunweb.pavithran@gmail.com <sunweb.pavithran@gmail.com>
2019-08-07 10:44:13 +0530

    Updates

---
+++ precache-manifest.5afa28a64ca7eb138465e8b9408da87f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "766cf60f9927f313cecf128a4b47a7c4",
+    "url": "/test-react/index.html"
+  },
+  {
+    "revision": "c53223341ab0b7eeccd0",
+    "url": "/test-react/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "0f5dba7278566abd30fe",
+    "url": "/test-react/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "c53223341ab0b7eeccd0",
+    "url": "/test-react/static/js/2.dfefd1a2.chunk.js"
+  },
+  {
+    "revision": "0f5dba7278566abd30fe",
+    "url": "/test-react/static/js/main.1b97f0d7.chunk.js"
+  },
+  {
+    "revision": "31d3ffe18f2cb45b7bd6",
+    "url": "/test-react/static/js/runtime~main.bea1702e.js"
+  }
+]);
